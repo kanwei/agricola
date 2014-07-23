55e57c1c792c67a5aa3648cdbf6551c77d9c6f60
Kanwei Li <kanwei@gmail.com>
2014-07-23 02:45:15 -0400

    Initial

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to agricola
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/agricola/core_test.clj
@@ -0,0 +1,7 @@
+(ns agricola.core-test
+  (:require [clojure.test :refer :all]
+            [agricola.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
